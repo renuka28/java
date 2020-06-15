ecc0673e40d2143c2b2af037f331291768be2d3e
renuka28 <mr.renukaprasad@gmail.com>
2020-06-15 16:36:30 -0500

    Basic Java Hibernate demo

--- hibernate_demo/hibernate_demo/src/org/renuka/learn/java/hibernate/dto/UserDetails.java
+++ hibernate_demo/hibernate_demo/src/org/renuka/learn/java/hibernate/dto/UserDetails.java
@@ -3,10 +3,11 @@ package org.renuka.learn.java.hibernate.dto;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 
-
-@Entity(name = "USER_DETAILS")
+@Entity
+@Table(name = "USER_DETAILS")
 public class UserDetails {
 	
 	public UserDetails(int userId, String userName) {
@@ -15,21 +16,26 @@ public class UserDetails {
 		this.userName = userName;
 	}
 	
-	@Id
-	@Column(name = "USER_ID")
+
 	private int userId;
 	
-	@Column(name = "USER_NAME")
+
 	private String userName;
 	
+	//we can have the annotation on the getter which will help us to modify the value if required
+	@Id
+	@Column(name = "USER_ID")
 	public int getUserId() {
 		return userId;
-	}
+	}	
+	
 	public void setUserId(int userId) {
 		this.userId = userId;
 	}
+	
+	@Column(name = "USER_NAME")
 	public String getUserName() {
-		return userName;
+		return userName + " updated using getter";
 	}
 	public void setUserName(String userName) {
 		this.userName = userName;
