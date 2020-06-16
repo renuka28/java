ba5391e641e448530413de98c1394014882d3774
renuka28 <mr.renukaprasad@gmail.com>
2020-06-15 22:01:05 -0500

    Basic Java Hibernate demo

--- hibernate_demo/src/org/renuka/learn/java/hibernate/HibernateTest.java
+++ hibernate_demo/src/org/renuka/learn/java/hibernate/HibernateTest.java
@@ -1,23 +1,122 @@
 package org.renuka.learn.java.hibernate;
 
+import java.util.Date;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-
+import org.renuka.learn.java.hibernate.dto.Address;
+import org.renuka.learn.java.hibernate.dto.Address2;
 import org.renuka.learn.java.hibernate.dto.UserDetails;
+import org.renuka.learn.java.hibernate.dto.UserDetails2;
+import org.renuka.learn.java.hibernate.dto.UserDetails3;
+import org.renuka.learn.java.hibernate.dto.UserDetails4;
 
 public class HibernateTest {
 
+	private static SessionFactory sessionFactory = null;
+	private static int currentUserId = 1;
 	public static void main(String[] args) {
-	
-		UserDetails user = new UserDetails(1, "First user");
 		
-		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		 
+		setupSessionFactoryDB();
+		demoInsert();
+		demoReterive();
+		demoPrimaryId();
+		demoEmbeddedValueType();
+		demoEmbeddedValueType2();
+
+	}
+	
+	public static void demoEmbeddedValueType2() {
+		
+		
+		System.out.println("inserting user details to user_details4 with embedded value types");		
+		Address2 homeAddress = new Address2("dummy home street", "home-tx", "home-la", "home-85151");		
+		Address2 officeAddress = new Address2("dummy office street", "office-tx", "office-la", "office-85151");
+		UserDetails4 user4 = new UserDetails4("user name", homeAddress, officeAddress);
+		System.out.println(user4.toString());
 		Session session = sessionFactory.openSession();
+		//save to db
+		session.beginTransaction();
+		session.save(user4);
+		session.getTransaction().commit();			
+		session.close();	
+		System.out.println("-----------------------------------------------------------------------------\n");
+	}
+	
+	public static void demoEmbeddedValueType() {
+		
+		
+		System.out.println("inserting user details to user_details3 with embedded value types");		
+		Address userAddress = new Address("dummy stree", "tx", "la", "85151");		
+		UserDetails3 user3 = new UserDetails3("user name", userAddress);
+		System.out.println(user3.toString());
+		Session session = sessionFactory.openSession();
+		//save to db
+		session.beginTransaction();
+		session.save(user3);
+		session.getTransaction().commit();			
+		session.close();	
+		System.out.println("-----------------------------------------------------------------------------\n");
+	}
+	
+	
+	public static void demoPrimaryId() {
+		
+		
+		System.out.println("inserting user details to user_details2 with auto id generation");
+		UserDetails2 user2 = new UserDetails2("First user", new Date(),"First user address", "first user description" );
+		Session session = sessionFactory.openSession();
+		//save to db
+		session.beginTransaction();
+		session.save(user2);
+		session.getTransaction().commit();	
+		System.out.println(user2.toString());		
+		session.close();	
+		System.out.println("-----------------------------------------------------------------------------\n");
+	}
+	
+	public static void demoReterive() {
+
+		Session session = sessionFactory.openSession();		
+		
+		//read from db
+		session.beginTransaction();
+		
+		System.out.println("reteriving user details for user id " + currentUserId);		
+		UserDetails user = (UserDetails)session.get(UserDetails.class, currentUserId);		
+		System.out.println(user.toString());
+		
+		session.getTransaction().commit();	
+		session.close();
+		System.out.println("-----------------------------------------------------------------------------\n");
+		
+	}
+	
+	public static void demoInsert() {
+		
+		
+		System.out.println("inserting user details for user id " + currentUserId);
+		UserDetails user = new UserDetails(currentUserId, "First user", new Date(),"First user address", "first user description" );
+		Session session = sessionFactory.openSession();
+		//save to db
 		session.beginTransaction();
 		session.save(user);
 		session.getTransaction().commit();	
+		session.close();	
+		System.out.println("-----------------------------------------------------------------------------\n");
+	}
+	
+	public static void setupSessionFactoryDB() {
+		
+		sessionFactory = new Configuration().configure().buildSessionFactory();
+		System.out.println("\n-----------------------------------------------------------------------------\n");
+		System.out.println("connecting to database using url : "
+					+ sessionFactory.getProperties().get("hibernate.connection.url"));
+		System.out.println("-----------------------------------------------------------------------------\n");
 
+	
 	}
 
 }
---
+++ hibernate_demo/src/org/renuka/learn/java/hibernate/dto/UserDetails4.java
@@ -0,0 +1,85 @@
+package org.renuka.learn.java.hibernate.dto;
+
+import java.util.Date;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+
+
+@Entity
+@Table(name = "USER_DETAILS4")
+public class UserDetails4 {
+	
+	@Id @GeneratedValue(strategy=GenerationType.AUTO)
+	private int userId;
+	private String userName;	
+	@Embedded
+	@AttributeOverrides(value = {  
+		@AttributeOverride(name="street", column=@Column(name="HOME_STREET")),
+		@AttributeOverride(name="city", column=@Column(name="HOME_CITY")),
+		@AttributeOverride(name="state", column=@Column(name="HOME_STATE")),
+		@AttributeOverride(name="pincode", column=@Column(name="HOME_PINCODE"))
+	})
+	private Address2 homeAddress;
+	@Embedded
+	@AttributeOverrides(value = {  
+			@AttributeOverride(name="street", column=@Column(name="OFFICE_STREET")),
+			@AttributeOverride(name="city", column=@Column(name="OFFICE_CITY")),
+			@AttributeOverride(name="state", column=@Column(name="OFFICE_STATE")),
+			@AttributeOverride(name="pincode", column=@Column(name="OFFICE_PINCODE"))
+		})
+	private Address2 officeAddress;
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public Address2 getHomeAddress() {
+		return homeAddress;
+	}
+	public void setHomeAddress(Address2 homeAddress) {
+		this.homeAddress = homeAddress;
+	}
+	public Address2 getOfficeAddress() {
+		return officeAddress;
+	}
+	public void setOfficeAddress(Address2 officeAddress) {
+		this.officeAddress = officeAddress;
+	}
+	@Override
+	public String toString() {
+		return "UserDetails4 [userId=" + userId + ", userName=" + userName + ", homeAddress=" + homeAddress
+				+ ", officeAddress=" + officeAddress + "]";
+	}
+	public UserDetails4(String userName, Address2 homeAddress, Address2 officeAddress) {
+		super();
+		this.userId = userId;
+		this.userName = userName;
+		this.homeAddress = homeAddress;
+		this.officeAddress = officeAddress;
+	}
+	
+	
+	
+	
+
+}
