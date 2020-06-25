8e6d1c0f59af1782627b18cc8a605e9b3ff7e2d9
renuka28 <mr.renukaprasad@gmail.com>
2020-06-25 10:19:07 -0500

    Spring security with JPA Authentication and MySQL - Starter project

---
+++ spring/spring_security/db-jpa-mysql/src/main/java/org/renuka/learn/java/security/dbjpamysql/DbJpaMysqlApplication.java
@@ -0,0 +1,13 @@
+package org.renuka.learn.java.security.dbjpamysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbJpaMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbJpaMysqlApplication.class, args);
+	}
+
+}
---
+++ spring/spring_security/db-jpa-mysql/src/test/java/org/renuka/learn/java/security/dbjpamysql/DbJpaMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package org.renuka.learn.java.security.dbjpamysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbJpaMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
